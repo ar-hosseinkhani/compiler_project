7c8fc68d96073ff0385c57ec3f79a3b72fd107c4
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-10-16 21:07:17 +0330

    define vars

--- compiler.py
+++ compiler.py
@@ -1,5 +1,42 @@
-# in the name of Allah
-# by Mohammad Cheraghi and Alireza Hosseinkhani
+from vars import *
 
 
-print("hello world")
+def get_next_token():
+    program = '''
+        void main ( void ) {
+        int a = 0;
+        // comment1
+        a = 2 + 2;
+        return;
+    }
+    '''
+
+    start = 0
+    forward = 0
+    line = 1
+    state = START
+    while True:
+        char = program[forward]
+        if char == '\n':
+            line += 1
+        state = get_next_state(state, char)
+        if state in Accepting_States:
+            token = create_token(state, program[start:forward+1], line)
+            program = program[forward+1:]
+            return token
+        else:
+            forward += 1
+
+
+def get_next_state(state, c):
+    if state == START:
+        if c in Blanks:
+            return WHS
+        elif c in Symbols:
+            return SYMBOL
+
+
+
+
+def create_token(final_state, lexeme, line_number):
+    return True
---
+++ vars.py
@@ -0,0 +1,28 @@
+import string
+
+START = 'start'
+LET1 = 'let1'
+LET2 = 'let2'
+DIG1 = 'dig1'
+DIG2 = 'dig2'
+BLANK = 'blank'
+SYMBOL = 'symbol'
+EQ1 = 'eq1'
+EQ2 = 'eq2'
+EQEQ = 'eqeq'
+CMT1 = 'cmt1'
+CMT2 = 'cmt2'
+CMT3 = 'cmt3'
+CMT4 = 'cmt4'
+CMTMF = 'cmtmf'
+CMTSF = 'cmtsf'
+CMTEF = 'cmtef'
+
+
+Blanks = ['\f', '\t', ' ', '\n', '\v', '\r']
+Symbols = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '<', ]
+Digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+Letters = [char for char in string.ascii_letters]
+Keywords = ['if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']
+
+Accepting_States = [LET2, DIG2, BLANK, SYMBOL, EQ2, EQEQ]
