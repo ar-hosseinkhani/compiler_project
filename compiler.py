f7c2377578b6492247289cceb8c19b370b03a596
ASUS <mohammad.cheraghi80@gmail.com>
2021-12-11 03:08:45 +0330

    debug

--- compiler.py
+++ compiler.py
@@ -3,88 +3,88 @@ from parser_vars import parser_data as data
 from rules import *
 from anytree import Node, RenderTree
 
-
-
 tree_list = []
 tree_list_father = []
 stack_list = []
-stack_list_index = []
+stack_list_father = []
 
-tree_list_father.append("-1")
-tree_list.append("Program")
 stack_list.append("Program")
-stack_list_index.append("0")
+stack_list_father.append("-1")
 data.set_next_token()
+
 
 def create_tree():
     tree = [Node("Program")]
-    for iter in range(1, len(tree_list)):
-        tree.append(Node(tree_list[iter], parent=tree[int(tree_list_father[iter])]))
+    for iteration in range(1, len(tree_list)):
+        tree.append(Node(tree_list[iteration], parent=tree[int(tree_list_father[iteration])]))
     return tree[0]
 
 
-while (True):
-    if data.lookahead.lexeme == '$':
-        break
+while True:
     node = stack_list.pop()
-    node_index = stack_list_index.pop()
+    father_index = stack_list_father.pop()
     node_rules = productions.get(node)
+    tree_list.append(node)
+    tree_list_father.append(father_index)
+    index = len(tree_list) - 1
+    look_ahead_lexeme = data.lookahead.lexeme
+    if look_ahead_lexeme == '$':
+        if node == '$':
+            break
+        else:
+            # Error
+            pass
+    if data.lookahead.type in ["NUM", "ID"]:
+        look_ahead_lexeme = data.lookahead.type
     if node in non_terminals:
         valid = False
         for product in node_rules:
-            if data.lookahead.lexeme in firsts.get(product[0]):
-                valid = True
-                for i in product[::-1]:
-                    tree_list.append(i)
-                    tree_list_father.append(node_index)
-                    stack_list.append(i)
-                    stack_list_index.append(len(tree_list) - 1)
-
+            for i in product:
+                if look_ahead_lexeme in firsts.get(i):
+                    valid = True
+                    for pr in product[::-1]:
+                        stack_list.append(pr)
+                        stack_list_father.append(index)
+                    break
+                elif 'epsilon' not in firsts.get(i):
+                    break
+            if valid:
+                break
         if not valid:
-            if data.lookahead.lexeme in follows.get(node):
+            if look_ahead_lexeme in follows.get(node):
                 found_eps = True
                 for product in node_rules:
+                    found_eps = True
                     for lex in product:
-                        if lex not in non_terminals:
-                            found_eps = False
-                            break
                         if 'epsilon' not in firsts.get(lex):
                             found_eps = False
                             break
                     if found_eps:
                         for i in product[::-1]:
-                            tree_list.append(i)
-                            tree_list_father.append(node_index)
                             stack_list.append(i)
-                            stack_list_index.append(len(tree_list) - 1)
+                            stack_list_father.append(index)
                         break
 
                 if not found_eps:
-                    if 'epsilon' in node_rules:
-                        tree_list.append('epsilon')
-                        tree_list_father.append(node_index)
-                    else:
-                        # missing node in line of lexeme
-                        pass
+                    # missing node in line of lexeme
+                    pass
                 pass
             else:
                 # illegal lexeme in line of lexeme
                 data.set_next_token()
                 stack_list.append(node)
-                stack_list_index.append(node_index)
+                stack_list_father.append(father_index)
 
     else:
-        if data.lookahead.lexeme == node:
-            tree_list[node_index] = '(' + data.lookahead.type + ', ' + node + ')'
+        if node == 'epsilon':
+            pass
+        elif look_ahead_lexeme == node:
+            tree_list[index] = '(' + data.lookahead.type + ', ' + data.lookahead.lexeme + ')'
             data.set_next_token()
         else:
             # missing node in line
-            tree_list[node_index] = '(' + data.lookahead.type + ', ' + node + ')'
+            tree_list[index] = '(' + data.lookahead.type + ', ' + data.lookahead.lexeme + ')'
             # data.set_next_token()
 
-
-    if data.lookahead.lexeme == '$':
-        break
-
 for pre, _, node in RenderTree(create_tree()):
-        print("%s%s" % (pre, node.name))
+    print("%s%s" % (pre, node.name))
